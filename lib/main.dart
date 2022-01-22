7f92f70a986f945a50d41102c91ff3e322aad414
Pranav2907 <71582082+Pranav2907@users.noreply.github.com>
2022-01-22 09:09:43 +0530

    Audio Integration Done

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:ai_radio/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+
+  // ignore: prefer_const_constructors
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: GoogleFonts.poppins().fontFamily
+      ),
+      // ignore: prefer_const_constructors
+      home: HomePage(),
+    );
+  }
+}
